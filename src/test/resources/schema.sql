745e1781c4848feb7371fb45132987ba982aa104
VincentCorgi <a325698741z@gmail.com>
2023-02-20 21:42:00 +0800

    test: 商品功能

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,13 @@
+-- 一定要加  IF NOT EXISTS 因為他會run好幾次，不要插入重複的
+CREATE TABLE IF NOT EXISTS product
+(
+    product_id         INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_name       VARCHAR(128)  NOT NULL,
+    category           VARCHAR(32)  NOT NULL,
+    image_url          VARCHAR(256) NOT NULL,
+    price              INT          NOT NULL,
+    stock              INT          NOT NULL,
+    description        VARCHAR(1024),
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+    );
