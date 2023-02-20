333f4cb7fd7bfbc7e87d919bbad8e26252ca911f
VincentCorgi <a325698741z@gmail.com>
2023-02-20 17:21:01 +0800

    feat: 查詢商品列表(基礎實作)

--- src/main/java/com/vincent/springbootmall/dao/ProductDao.java
+++ src/main/java/com/vincent/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.vincent.springbootmall.dao;
 import com.vincent.springbootmall.dto.ProductRequest;
 import com.vincent.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductByID(Integer productId);
 
--- src/main/java/com/vincent/springbootmall/service/ProductService.java
+++ src/main/java/com/vincent/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.vincent.springbootmall.service;
 import com.vincent.springbootmall.dto.ProductRequest;
 import com.vincent.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductByID(Integer productId);
 
